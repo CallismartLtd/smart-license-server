d1555ad0bb5db4e6bc676123b72b4b288ed4984f
Callismart Tech <admin@callismart.com.ng>
2024-05-27 00:11:46 +0100

    Update license-add.php

--- templates/forms/license-add.php
+++ templates/forms/license-add.php
@@ -15,13 +15,11 @@ defined( 'ABSPATH' ) ||  exit;
             <label for="user_id" class="smliser-form-label">Client</label>
             <span class="smliser-form-description" title="Add User">?</span>
             <?php
-            $selected_user = ( $user_id ) ? get_user_by( 'ID', $user_id ) : false;
             wp_dropdown_users(
                 array(
                     'name'              => 'user_id',
-                    'selected'          => $selected_user ? $selected_user->ID : '',
                     'show_option_none'  => esc_html__( 'Select a client', 'smliser' ),
-                    'option_none_value' => 0,
+                    'option_none_value' => -1,
                     'class'             => 'smliser-form-input',
                     'show'              => 'display_name_with_login',
                 )
@@ -33,21 +31,21 @@ defined( 'ABSPATH' ) ||  exit;
         <div class="smliser-form-row">
             <label for="service_id" class="smliser-form-label">Service ID</label>
             <span class="smliser-form-description" title="Service ID uniquely identifies a License key. It is required when accessing the license validation API endpoint">?</span>
-            <input type="text" class="smliser-form-input" name="service_id" id="service_id" value="<?php echo esc_attr( $license->get_service_id() ); ?>">
+            <input type="text" class="smliser-form-input" name="service_id" id="service_id" value="">
         </div>
 
         <!-- Item ID -->
         <div class="smliser-form-row">
             <label for="item_id" class="smliser-form-label">Item ID</label>
             <span class="smliser-form-description" title="Item ID uniquely identifies a License key. It is required when accessing the license validation API endpoint">?</span>
-            <input type="number" class="smliser-form-input" name="item_id" id="item_id" value="<?php echo esc_attr( $license->get_item_id() ); ?>">
+            <input type="number" class="smliser-form-input" name="item_id" id="item_id" value="">
         </div>
         
         <!-- Allowed Websites -->
         <div class="smliser-form-row">
             <label for="allowed_sites" class="smliser-form-label">Allowed Websites</label>
             <span class="smliser-form-description" title="Item ID uniquely identifies a License key. It is required when accessing the license validation API endpoint">?</span>
-            <input type="number" class="smliser-form-input" name="allowed_sites" id="allowed_sites" value="<?php echo esc_attr( $license->get_allowed_websites() ); ?>">
+            <input type="number" class="smliser-form-input" name="allowed_sites" id="allowed_sites" value="">
         </div>
 
         <!-- Status -->
@@ -55,11 +53,11 @@ defined( 'ABSPATH' ) ||  exit;
             <label for="status" class="smliser-form-label">Status</label>
             <span class="smliser-form-description" title="Select the license status">?</span>
             <select name="status" id="status" class="smliser-select-input">
-                <option value="" <?php selected( '', $license->get_status( 'edit' ) ); ?>><?php esc_html_e( 'Auto Calculate', 'smliser' ); ?></option>
-                <option value="Active" <?php selected( 'Active', $license->get_status( 'edit' ) ); ?>><?php esc_html_e( 'Active', 'smliser' ); ?></option>
-                <option value="Deactivated" <?php selected( 'Deactivated', $license->get_status( 'edit' ) ); ?>><?php esc_html_e( 'Deactivated', 'smliser' ); ?></option>
-                <option value="Expired" <?php selected( 'Expired', $license->get_status( 'edit' ) ); ?>><?php esc_html_e( 'Expired', 'smliser' ); ?></option>
-                <option value="Suspended" <?php selected( 'Suspended', $license->get_status( 'edit' ) ); ?>><?php esc_html_e( 'Suspended', 'smliser' ); ?></option>
+                <option value=""><?php esc_html_e( 'Auto Calculate', 'smliser' ); ?></option>
+                <option value="Active"><?php esc_html_e( 'Active', 'smliser' ); ?></option>
+                <option value="Deactivated"><?php esc_html_e( 'Deactivated', 'smliser' ); ?></option>
+                <option value="Expired"><?php esc_html_e( 'Expired', 'smliser' ); ?></option>
+                <option value="Suspended"><?php esc_html_e( 'Suspended', 'smliser' ); ?></option>
             </select>
         </div>
 
@@ -67,16 +65,16 @@ defined( 'ABSPATH' ) ||  exit;
         <div class="smliser-form-row">
             <label for="start_date" class="smliser-form-label">Start Date</label>
             <span class="smliser-form-description" title="Choose the commencement date for this license">?</span>
-            <input type="date" class="smliser-form-input" name="start_date" id="start_date" value="<?php echo esc_attr( $license->get_start_date() ); ?>">
+            <input type="date" class="smliser-form-input" name="start_date" id="start_date" value="">
         </div>
 
         <!-- End Date -->
         <div class="smliser-form-row">
             <label for="end_date" class="smliser-form-label">End Date</label>
             <span class="smliser-form-description" title="Choose the expiration date for this license">?</span>
-            <input type="date" class="smliser-form-input" name="end_date" id="end_date" value="<?php echo esc_attr( $license->get_end_date() ); ?>">
+            <input type="date" class="smliser-form-input" name="end_date" id="end_date" value="">
         </div>
 
-        <input type="submit" name="smliser_license_update" class="button action smliser-bulk-action-button" value="Update">
+        <input type="submit" name="smliser_license_new" class="button action smliser-bulk-action-button" value="Add License">
     </form>
 </div>
