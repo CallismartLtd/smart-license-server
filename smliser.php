3b339fc7f78e8ab4790a790dcd9af7486824f106
Callismart Tech <admin@callismart.com.ng>
2024-10-03 17:37:14 +0100

    Update smliser.php

--- smliser.php
+++ smliser.php
@@ -5,7 +5,7 @@
  * Description: license REST API server for WordPress premium plugins.
  * Author: Callistus Nwachukwu
  * Author URI: https://callismart.com.ng/callistus
- * Version: 1.0
+ * Version: 0.0.1
  */
 defined( 'ABSPATH' ) || exit;
 
@@ -15,7 +15,7 @@ if ( defined( 'SMLISER_PATH' ) ) {
 
 define( 'SMLISER_PATH', __DIR__ . '/' );
 define( 'SMLISER_FILE', __FILE__ );
-define( 'SMLISER_VER', '1.0.0' );
+define( 'SMLISER_VER', '0.0.1' );
 define( 'SMLISER_URL', plugin_dir_url( __FILE__) );
 
 require_once SMLISER_PATH . 'includes/class-smliser-config.php';
