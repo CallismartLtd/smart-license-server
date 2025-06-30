30177c95f6cf1a9e85cdebc5928d6eca87e6db19
Callismart Tech <admin@callismart.com.ng>
2025-06-30 14:10:30 +0100

    Create class-smliser-repository-rest-api.php

---
+++ includes/smliser-rest-api/class-smliser-repository-rest-api.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The Repository REST API class.
+ * 
+ * @author Callistus Nwachukwu
+ * @package Smliser\classes
+ * @since 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+class Smliser_Repository_Rest_API {
+    
+}
