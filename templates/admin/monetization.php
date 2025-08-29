0ab5c525856caf8cdfba79ec696137b9eaee943a
Callismart Tech <admin@callismart.com.ng>
2025-08-29 15:06:43 +0100

    Update monetization.php

--- templates/admin/monetization.php
+++ templates/admin/monetization.php
@@ -8,15 +8,19 @@
  * @since 0.0.5 
  */
 
-use SmartLicenseServer\Monetization\Monetization;
+use SmartLicenseServer\Monetization\Monetization,
+    SmartLicenseServer\Monetization\Provider_Collection;
+
+;
 
 defined( 'ABSPATH' ) || exit; 
 
 $id         = smliser_get_query_param( 'item_id' );
-$item_type  = smliser_get_query_param( 'item_type' );
+$item_type  = smliser_get_query_param( 'type' );
 $is_new     = false;
 
 $object     = Monetization::get_by_item( $item_type, $id );
+$providers  = Provider_Collection::instance()->get_providers();
 
 if ( empty( $object ) ) {
     $is_new = true;
@@ -25,12 +29,13 @@ if ( empty( $object ) ) {
         ->set_item_type( $item_type );
 }
 
+$item_object = $object->get_item_object();
 ?>
 
 <h1>Software Monetization</h1>
 
-<?php if ( empty( $object ) ) : ?>
+<?php if ( empty( $item_object ) ) : ?>
     <?php echo wp_kses_post( smliser_not_found_container( 'Repository item does not exists <a href="' . smliser_repo_page() . '">Back</a>' ) ); ?>
 <?php else : ?>
-
+<p><span class="dashicons dashicons-info"></span><?php echo esc_html( $item_object->is_monetized() ? sprintf() : sprintf( 'Use this interface to set up monetization for %s', $item_object->get_name() ) ); ?></p>
 <?php endif; ?>
