49aa9c5d583c3329031f9b0e914c6e513c3fb81d
Callistus Chimezie Nwachukwu <admin@callismart.com.ng>
2025-10-28 10:36:44 +0100

    Update smliser.php

--- smliser.php
+++ smliser.php
@@ -2,10 +2,10 @@
 /*
  * Plugin Name: Smart license Server
  * Plugin URI: https://callismart.com.ng/smart-license-server
- * Description: license REST API server for WordPress premium plugins.
+ * Description: Private plugin, themes and software repository with licensing and monetization feature.
  * Author: Callistus Nwachukwu
  * Author URI: https://callismart.com.ng/callistus
- * Version: 0.0.9
+ * Version: 0.1.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -16,9 +16,9 @@ if ( defined( 'SMLISER_PATH' ) ) {
 
 define( 'SMLISER_PATH', __DIR__ . '/' );
 define( 'SMLISER_FILE', __FILE__ );
-define( 'SMLISER_VER', '0.0.9' );
+define( 'SMLISER_VER', '0.1.0' );
 define( 'SMLISER_DB_VER', '0.0.9' );
-define( 'SMLISER_URL', plugin_dir_url( __FILE__) );
+define( 'SMLISER_URL', plugin_dir_url( __FILE__ ) );
 
 require_once SMLISER_PATH . 'includes/class-smliser-config.php';
 require_once SMLISER_PATH . 'includes/class-install.php';
