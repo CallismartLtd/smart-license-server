3d3b3627f9ea9328456ecd84aac35da3ce4f97e9
CallismartLtd <admin@callismart.com.ng>
2026-01-05 23:28:12 +0100

    Introduced token delivery trait

--- includes/Monetization/class-DownloadToken.php
+++ includes/Monetization/class-DownloadToken.php
@@ -13,6 +13,7 @@ use SmartLicenseServer\Exceptions\Exception;
 use SmartLicenseServer\HostedApps\AbstractHostedApp;
 use SmartLicenseServer\Monetization\License;
 use SmartLicenseServer\Utils\CommonQueryTrait;
+use SmartLicenseServer\Utils\TokenDeliveryTrait;
 
 defined( 'SMLISER_ABSPATH' ) || exit;
 
@@ -21,7 +22,7 @@ defined( 'SMLISER_ABSPATH' ) || exit;
  * to hosted applications in the repository.
  */
 class DownloadToken {
-    use CommonQueryTrait;
+    use CommonQueryTrait, TokenDeliveryTrait;
     /**
      * Token ID.
      * 
@@ -422,42 +423,4 @@ class DownloadToken {
 
         return $self;
     }
-
-
-    /**
-     * Derive a secure key using HKDF with WordPress salts.
-     *
-     * @return string 32-byte encryption key.
-     */
-    public static function derive_key() {
-        $secret = defined( 'SECURE_AUTH_KEY' ) ? SECURE_AUTH_KEY : 'zNqTILD04P3jQ8$Ev[v$Tec[Wf*(6>RxYukbrv{|qUOUr>($1@uO:ur%*<VX@:]&';
-        $salt   = defined( 'SECURE_AUTH_SALT' ) ? SECURE_AUTH_SALT : '7RY@sFBq:]9?JR`21~_45]$Iy.*bF8bO4(L{Ks9(yBxur +i=3D9@`b-fRF_K8JJ';
-
-        return hash_hkdf( 'sha256', $secret, 32, '', $salt );
-    }
-
-    /**
-     * Encode a string to URL-safe Base64.
-     *
-     * @param string $data
-     * @return string
-     */
-    private static function base64url_encode( string $data ) : string {
-        return rtrim( strtr( base64_encode( $data ), '+/', '-_' ), '=' );
-    }
-
-    /**
-     * Decode a URL-safe Base64 string.
-     *
-     * @param string $data
-     * @return string
-     */
-    private static function base64url_decode( string $data ) : string {
-        $padding = 4 - ( strlen( $data ) % 4 );
-        if ( $padding < 4 ) {
-            $data .= str_repeat( '=', $padding );
-        }
-        return base64_decode( strtr( $data, '-_', '+/' ) );
-    }
-
 }
