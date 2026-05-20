32db7b5f0176055a95abc88774781d3dae9432d5
CallismartLtd <admin@callismart.com.ng>
2026-05-20 18:01:26 +0100

    Removed database abstraction layer to favour callismart/dbprism

--- src/Database/Query/Renderers/PostgreSQLCompositeRenderer.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace SmartLicenseServer\Database\Query\Renderers;
-
-/**
- * PostgreSQL Composite Renderer.
- */
-class PostgreSQLCompositeRenderer extends MySQLCompositeRenderer {}
--- src/Database/Query/Renderers/SQLiteCompositeRenderer.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace SmartLicenseServer\Database\Query\Renderers;
-
-/**
- * SQLite Composite Renderer.
- */
-class SQLiteCompositeRenderer extends MySQLCompositeRenderer {}
