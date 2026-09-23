8df98253a95f14e05aaa77649dd39771e9afabb7
CallismartLtd <admin@callismart.com.ng>
2026-09-23 15:14:43 +0100

    fix: Remove deprecated annotation from url function documentation

--- src/Utils/functions/url-functions.php
+++ src/Utils/functions/url-functions.php
@@ -14,7 +14,6 @@ use SmartLicenseServer\Exceptions\FileRequestException;
  * @param string $path Path(optional).
  * @param array<string, string> $params Associative array of query params.
  * 
- * @deprecated
  * @return URL
  */
 function url( string $path = '', array $params = [] ) : URL {
